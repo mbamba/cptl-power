3a05eca11097e4d605acc4d521ecb1bce56a96ab
Gabe Weaver <gweaver@illinois.edu>
2014-07-01 11:01:12 -0500

    changes to DAO

--- src/cptl/__init__.py
+++ src/cptl/__init__.py
@@ -20,253 +20,196 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/
 """
 import libxml2
+import networkx as nx
+from networkx.readwrite import json_graph
 
 class NMapDAO():
 
     # Identifier for the group of networks (e.g. enterprise name)
-    network_group = None
-    network_id = None
     xml_nmap_path = None
-    error_code = None
-    nmap_xml_doc = None
-    nmap_ns = None
-    graphml_ns = None
-
-    host_id_prefix = "HOST_"
+    cptl_nmap_graph = None
+    host_id_prefix = None
 
     """
     Factory method for the nmap DAO.  For now we take a path to 
       an NMap XML file as input.
-    @param xml_nmap_path 
-    @param network_group  ID for the group of networks (e.g. enterprise name)
-    @param network_id     ID for a network being scanned
     @return an NMapDAO object.
     """
     @staticmethod
-    def create(xml_nmap_path, network_group, network_id):
+    def create(nmap_input_data_path,\
+                   nmap_input_data_format,\
+                   network_urn,\
+                   host_id_prefix):
         nmDAO = NMapDAO()
         try: 
-            nmDAO.network_group = network_group
-            nmDAO.network_id = network_id
-            nmDAO.xml_nmap_path = xml_nmap_path
-            nmDAO.nmap_xml_doc = libxml2.parseFile(xml_nmap_path)
+            nmDAO.host_id_prefix = host_id_prefix
+            nmDAO.initializeCPTLGraph(nmap_input_data_path,\
+                                          nmap_input_data_format,\
+                                          network_urn)
         except Exception as e:
             print e
         return nmDAO
-    
+
+    """
+    Given a path to the XML output of nmap, initialize 
+    the nmap_graph for this instance.  
+    @param input_type is NMAP_XML or CPTL_NMAP_XML
+    @param input_data_path  Path to XML output of NMap Scan
+      self.nmap_graph is initialized
+    """
+    def initializeCPTLGraph(self, input_data_path, input_data_format, network_urn):
+        self.cptl_nmap_graph = nx.Graph()
+        self.cptl_nmap_graph.graph['network_urn'] = network_urn
+
+        if "CPTL_NMAP_XML" == input_data_format:
+            raise Exception("CPTL_NMAP_XML parser not yet implemented!")
+        elif "NMAP_XML" == input_data_format:
+            self._initializeCPTLGraphFromNMAPXML(input_data_path)
+
+    def _initializeCPTLGraphFromNMAPXML(self, input_data_path):
+        nmap_xml_doc = libxml2.parseFile(input_data_path)
+
+        # Parse information for the scanned network
+        context = nmap_xml_doc.xpathNewContext()
+        finished_element = context.xpathEval("//finished")[0]
+        self.cptl_nmap_graph.graph['timestr'] = finished_element.prop("timestr")
+        self.cptl_nmap_graph.graph['summary'] = finished_element.prop("summary")
+        
+        # Parse information for the hosts
+        host_elements = context.xpathEval("//host")
+        ref2idx = {}
+        id2host = { 1:"linksys", 2:"switch", 3:"whopper", 4:"beth-hp",\
+                        5:"printer", 6:"wills-gateway", 7:"winders8", 8:"bigslaptop" }
+        i = 1
+        for host_element in host_elements:
+            host_id = self.host_id_prefix + str(i)
+            host_ref = self.cptl_nmap_graph.graph['network_urn'] + ":" + id2host[i] #host_id
+            self.cptl_nmap_graph.add_node(i, urn_id=host_ref)
+            
+
+            #-- now add attribute information
+            self.cptl_nmap_graph.node[i]['asset_type_urn'] =\
+                "urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST"
+            
+            child = host_element.children
+            while child is not None:
+                if "element" == child.type and "address" == child.name:
+                    addrtype = child.prop("addrtype")
+                    if "ipv4" == addrtype:
+                        self.cptl_nmap_graph.node[i]['urn-cptl-HOST-ipv4'] = child.prop("addr")
+                    elif "ipv6" == addrtype:
+                        self.cptl_nmap_graph.node[i]['urn-cptl-HOST-ipv6'] = child.prop("addr")
+                    elif "mac" == addrtype:
+                        self.cptl_nmap_graph.node[i]['urn-cptl-HOST-mac'] = child.prop("addr")
+                        
+                child = child.next
+            i = i + 1
+
+        # No information to parse for the edges
+        nmap_xml_doc.freeDoc()
+        context.xpathFreeContext()
+
+    """
+    def initializeCPTLGraphFromCPTLXML(self, input_data_path):
+        cptl_xml_doc = libxml2.parseFile(input_data_path)
+
+        # Parse information for the scanned network
+        context = cptl_xml_doc.xpathNewContext()
+        graph_element = context.xpathEval("//graph")[0]
+        graph_ref = graph_element.prop("id")
+        if (self.cptl_graph['graph_ref'] != graph_ref):
+            throw Exception("Graph refs don't match!")
+            
+        data_timestr = context.xpathEval("//graph/data[@key='timestr']")[0]
+        self.cptl_graph['timestr'] = data_timestr.getContent()
+        data_summary = context.xpathEval("//graph/data[@key='summary']")[0]
+        self.cptl_graph['summary'] = data_summary.getContent()
+
+        # Parse information for the hosts
+        host_elements = context.xpathEval("//host")
+        i = 1
+        for host_element in host_elements:
+            host_id = self.host_id_prefix + str(i)
+            host_ref = self.cptl_graph['graph_ref'] + "." + host_id
+            self.cptl_graph.add_node(host_ref)
+            i = i + 1
+            
+        # Parse information for the edges
+        edge_elements = context.xpathEval("//edge")
+        i = 1
+        for edge_element in edge_elements:
+            source_host_id = edge_element.prop("source")
+            target_host_id = edge_element.prop("target")
+            source_host_ref = self.cptl_graph['graph_ref'] + "." + source_host_id
+            target_host_ref = self.cptl_graph['graph_ref'] + "." + target_host_id
+            self.cptl_graph.add_edge(source_host_ref, target_host_ref)
+            i = i + 1
+
+        cptl_xml_doc.freeDoc()
+        context.xpathFreeContext()
+    """
+
     """
     Retrieve all available types of assets for the given nmap scan.
       eventually incorporate a context parameter.
     @return A list of references 
     """
-    def getAssetTypes(self):
-        self.error_code = 0
-        refs = [ ("urn:nmap:network.host", "A host within the network"),
-                 ("urn:nmap:network.host#status", "The status of a network host"),
-                 ("urn:nmap:network.host#address", "The address of a network host"),
-                 ("urn:nmap:network.host#hostnames", "The hostnames of a network host"),
-                 ("urn:nmap:network.host#ports", "The ports open on a network host"),
-                 ("urn:nmap:network.host#os", "The OS of a network host") ]
-        return refs
+    @staticmethod
+    def getAssetTypeURNs():
+        asset_type_urns = [ ("urn:nmap:NAMESPACE:NETWORKS.NETWORK",\
+                                 "urn:nmap:egCorp:corporate.core",\
+                                 "A network"), 
+                            ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST",\
+                                 "urn:nmap:egCorp:corporate.core:HOST_1",\
+                                 "A host within the network") ]
+        return asset_type_urns
+
+    """
+    Given an asset type, retrieve the attributes associated with that
+      type.
+    @param asset_type The type of asset for which to retrieve attributes
+    @return a list of type attribute tuples (attribute_ref, attribute description)
+    """
+    @staticmethod
+    def getAssetAttributeTypeURNs(asset_type_urn):
+        asset_attr_type_urns = []
+        if "urn:nmap:NAMESPACE:NETWORKS.NETWORK" == asset_type_urn:
+            asset_attr_type_urns = [ ("urn:nmap:NAMESPACE:NETWORKS.NETWORK@timestr", "The time of the network scan"),
+                                 ("urn:nmap:NAMESPACE:NETWORKS.NETWORK@summary", "The summary of the nmap scan") ]
+        elif "urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST" == asset_type_urn:
+            asset_attr_type_urns = [ ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST#status", "The status of a network host"),
+                                 ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST#address", "The address of a network host"),
+                                 ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST#hostnames", "The hostnames of a network host"),
+                                 ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST#ports", "The ports open on a network host"),
+                                 ("urn:nmap:NAMESPACE:NETWORKS.NETWORK:HOST#os", "The OS of a network host") ]
+        else:
+            raise Exception("Unknown asset type: " + asset_type_urn)
+
+        return asset_attr_type_urns
+
+    def getCPTLGraph(self, output_format=None):
+        result = None
+        if None == output_format:
+            result = self.cptl_nmap_graph
+        elif "JSON" == output_format:
+            result = json_graph.dumps(self.cptl_nmap_graph)
+        else:
+            raise Exception("Output format: " + output_format + "not yet supported")
+        return result
 
     """
     Retrieve all instances of an asset of a given type
     @param asset_type_ref The type of asset to retrieve
     @return A list of asset references
-    """
-    def getAssetRefs(self, asset_type_ref):
-        asset_refs = []
+
+    def getAssetRefs(self, asset_type_urn):
+        asset_urns = []
         if "urn:nmap:network" == asset_type_ref:
-            network_asset_urn = "urn:" + self.network_group + ":" + self.network_id
+            network_asset_urn = self.cptl_nmap_graph.graph['graph_ref']
             asset_refs.append(network_asset_urn)
         elif "urn:nmap:network.host" == asset_type_ref:
-            asset_refs = self._getNetworkHostRefs()
-        elif "urn:nmap:network.host#status" == asset_type_ref or\
-                "urn:nmap:network.host#address" == asset_type_ref or\
-                "urn:nmap:network.host#hostnames" == asset_type_ref or\
-                "urn:nmap:network.host#ports" == asset_type_ref or\
-                "urn:nmap:network.host#os" == asset_type_ref:
-            asset_refs = self._getNetworkHostPropertyRefs(asset_type_ref)
+            asset_refs = self.cptl_nmap_graph.nodes()
         else:
             raise Exception("Asset reference retrieval not yet implemented for: " , asset_type_ref)
         return asset_refs
-
-    def getAsset(self, asset_vars):
-        assets = []
-
-        for asset_type_ref, asset_ref in asset_vars:     
-            if "urn:nmap:network.host" == asset_type_ref or\
-                    "urn:nmap:network.host#status" == asset_type_ref or\
-                    "urn:nmap:network.host#address" == asset_type_ref or\
-                    "urn:nmap:network.host#hostnames" == asset_type_ref or\
-                    "urn:nmap:network.host#ports" == asset_type_ref or\
-                    "urn:nmap:network.host#os" == asset_type_ref:
-                hosts = self._getNetworkHostRefs()
-                host_id = asset_ref.split(".")[1]  # Assume host id aligned with doc order
-                host_id = host_id.split("#")[0]
-                host_id = int(host_id.split("_")[1])
-                asset_xml = self._getNetworkHostPropertyNMapXML(asset_type_ref, host_id)
-                assets.append( (asset_type_ref, asset_ref, asset_xml) )
-            else:
-                raise Exception("Asset retrieval not yet implemented for: ", asset_type_ref)
-        return assets
-
-    def getAssetAsCPTLXML(self, asset_vars):
-        assets = []
-        
-        asset_type = asset_vars[0][0]
-        asset_ref = asset_vars[0][1]
-        if "urn:nmap:network" != asset_type:
-            raise Exception("unimplemented asset serialization to CPTL for type: " + asset_type)
-        cptl_doc = libxml2.newDoc("1.0")
-        graphml_element = cptl_doc.newChild(None, "graphml", None)
-        self.graphml_ns = graphml_element.newNs("http://graphml.graphdrawing.org/xmlns", "graphml")
-        self.nmap_ns = graphml_element.newNs("http://www.nmap.org/docs/2/nmap.dtd", "nmap")
-        
-        # Set up the keys
-        for key_label in ["address", "hostnames", "label", "os", "ports", "status", "type"]:
-            self._createCPTLXMLKey(key_label, graphml_element, "node")
-        
-        # Now create the graph
-        graph_elet = graphml_element.newChild(None, "graph", None)
-        graph_elet.setProp("id", asset_ref)
-        graph_elet.setProp("edgedefault", "undirected")
-        
-        # Add some time information
-        graph_time_elet = graph_elet.newChild(None, "data", None)
-        graph_time_elet.setProp("key", "timestr")
-        graph_time_elet.setContent("Fri Sep 9 18:33:54 2011 (HARDCODED!)")
-
-        # Now add the Nodes
-        host_type = "urn:nmap:network.host"
-        host_refs = self.getAssetRefs(host_type)
-        for host_ref in host_refs:
-            self._createCPTLXMLHostElet(host_type, host_ref, graph_elet)
-            
-        return cptl_doc
-        
-    def _createCPTLXMLKey(self, key_label, parent_elet, attr_type):
-        key_elet = parent_elet.newChild(None, "key", None)
-        key_elet.setProp("id", key_label)
-        key_elet.setProp("for", attr_type)
-        key_elet.setProp("attr.name", key_label)
-        
-    def _createCPTLXMLHostElet(self, host_type, host_ref, parent_elet):
-        node_elet = parent_elet.newChild(None, "node", None)
-        node_elet.setProp("id", host_ref)
-        
-        node_type_elet = node_elet.newChild(None, "data", None)
-        node_type_elet.setProp("key", "type")
-        node_type_elet.setContent("network:Host")
-        
-        for key_label in ["os", "address", "status", "ports", "hostnames"]:
-            host_property_type = host_type + "#" + key_label
-            data_elet = node_elet.newChild(None, "data", None)
-            data_elet.setProp("key", key_label)
-            asset_vars = [ (host_property_type, host_ref) ]
-            asset_xml = self.getAsset(asset_vars)[0][2]
-            if asset_xml != None:
-                data_elet.addChild(asset_xml)
-
-    """
-    @return A list of assets of type 
-    """
-    def _getNetworkHostRefs(self):
-        host_refs = []
-        host_elements = self._getNetworkHostsNMapXML()
-        
-        i = 1
-        for host in host_elements:
-            host_id = self.host_id_prefix + str(i)
-            host_asset_urn = "urn:" + self.network_group + ":" + self.network_id + "." + host_id
-            host_refs.append(host_asset_urn)
-            i = i + 1
-        return host_refs
-
-    """
-    @return A list of properties of type "urn:nmap:network.host"
-    """
-    def _getNetworkHostPropertyRefs(self, asset_type_ref):
-        host_property_refs = []
-        host_refs = self._getNetworkHostRefs()
-        property = asset_type_ref.split("#")[1]   # A little sloppy here
-        for host_ref in host_refs:
-            host_property_ref = host_ref + "#" + property
-            host_property_refs.append(host_property_ref)
-        return host_property_refs
-
-    def _getNetworkHostsNMapXML(self):
-        self.nmap_xml_doc = libxml2.parseFile(self.xml_nmap_path)
-        context = self.nmap_xml_doc.xpathNewContext()
-        host_elements = context.xpathEval("//host")
-        self.nmap_xml_doc.freeDoc()
-        context.xpathFreeContext()
-        return host_elements
-
-    def _getNetworkHostPropertyNMapXML(self, asset_type_ref, host_id):
-        self.nmap_xml_doc = libxml2.parseFile(self.xml_nmap_path)
-        context = self.nmap_xml_doc.xpathNewContext()
-        xpath = self._getXPathForAssetType(asset_type_ref, host_id)
-        results = context.xpathEval(xpath)
-        if len(results) > 0:
-            host_property_element = context.xpathEval(xpath)[0].copyNode(1)
-            host_property_element.setNs(self.nmap_ns)
-            print [xpath, host_property_element]
-        else:
-            host_property_element = None
-        self.nmap_xml_doc.freeDoc()
-        context.xpathFreeContext()
-        return host_property_element
-        
-    def _getXPathForAssetType(self, asset_type_ref, host_id):
-        xpath = None
-        if "urn:nmap:network" == asset_type_ref:
-            xpath = "/"
-        elif "urn:nmap:network.host" == asset_type_ref:
-            xpath = "//host"
-        elif "urn:nmap:network.host#status" == asset_type_ref or\
-                "urn:nmap:network.host#address" == asset_type_ref or\
-                "urn:nmap:network.host#hostnames" == asset_type_ref or\
-                "urn:nmap:network.host#ports" == asset_type_ref or\
-                "urn:nmap:network.host#os" == asset_type_ref:
-            property = asset_type_ref.split("#")[1]
-            xpath = "//host[" + str(host_id) + "]/" + property
-        else:
-            raise Exception("XPath for asset type not yet implemented for: ", asset_type_ref)
-        return xpath
-
-    """
-    Retrieve all instances of an asset given by the scan
-    @param asset_type_urn    A urn reference to an asset provided by NMap
-    @param additional_params  A query object
-    @return A list of references to the hosts found within the scan
-
-    def getAssets(self, asset_type_urn, additional_params):
-        if "urn:nmap:network.host" == nmap_urn:
-            host_asset_urns = getNetworkHosts()
-            return host_asset_urns
-        elif "urn:nmap:network.host#status" == nmap_urn:
-            host_asset_urn = additional_params['urn:nmap:network.host']
-            return getNetworkHostProperty(host_asset_urn, asset_type_urn)
-        else:
-            raise Exception("Unrecognized asset exception", asset_urn)
-
-
-
-    @return a list of properties contained within "urn:nmap:network.host"
-
-    def getNetworkHostPropeties(self, host_asset_ref, asset_type_ref):
-        asset_type_refs = self.getAssetTypeRefs()
-
-        if not (asset_type_ref in asset_type_refs):
-            return []
-        elif "urn:nmap:network.host#status" == asset_type_ref:
-            properties = "Awesome JSON"
-        elif "urn:nmap:network.host#address" == asset_type_ref:
-            properties = "Awesome JSON"
-        else:
-            raise Exception("Not yet implemented", asset_type_ref)
-        return properties
-
-
     """
